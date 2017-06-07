5ae6ce71e814b7c48bfcaf407d40a00e9c50cafc
RafaelVilaca <ferever.2012@gmail.com>
2017-06-07 18:10:47 -0300

    DDD
    Projeto DDD EF

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.MVC.ViewModels;
+
+namespace ProjetoModeloDDD.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName => "ViewModelToDomainMappings";
+
+        protected override void Configure()
+        {
+            //Pra usar o Create Map, terá q ser uma versão abaixo da 4.2
+            //Install-Package AutoMapper -Version 3.2.1
+            Mapper.CreateMap<ClienteViewModel, Cliente>();
+            Mapper.CreateMap<ProdutoViewModel, Produto>();
+        }
+    }
+}
---
+++ ProjetoModeloDDD/ProjetoModeloDDD.MVC/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoModeloDDD.MVC.ViewModels
+{
+    public class ClienteViewModel
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Preencha o Campo Nome")]
+        [MaxLength(150, ErrorMessage = "Máximo de {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Mínimo de {0} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o Campo Sobrenome")]
+        [MaxLength(150, ErrorMessage = "Máximo de {0} caracteres")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o Campo Email")]
+        [MaxLength(100, ErrorMessage = "Máximo de {0} caracteres")]
+        [EmailAddress(ErrorMessage = "Preencha com um E-mail válido")]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public virtual IEnumerable<ProdutoViewModel> Produtos { get; set; }
+
+    }
+}
